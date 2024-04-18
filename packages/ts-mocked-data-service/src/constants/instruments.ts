126e750f9d984efc2982b6d8b2e86b10cd233788
Veneta Grigorova <veneta.grigorova@tick42.com>
2024-04-18 17:11:55 +0300

    renaming ticker

--- packages/ts-mocked-data-service/src/constants/instruments.ts
+++ packages/ts-mocked-data-service/src/constants/instruments.ts
@@ -22,7 +22,7 @@ export const instruments: InstrumentInfo[] = [
   },
 
   {
-    ticker: 'BCS',
+    ticker: 'BARC',
     bbgExchange: 'LN',
     isin: 'GB0031348658',
     description: 'Barclays Plc',
